5f27e82285c35ec6828b96b7026c490c90f1cd17
jakegodsall <jake.edward.godsall@gmail.com>
2022-06-20 20:50:04 +0100

    Added "delete" and "clear" buttons to the JFrame

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator implements ActionListener {
     JButton decButton, eqButton, delButton, clrButton;
     JPanel panel;
 
-    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    Font myFont = new Font("Ink Free", Font.BOLD, 20);
 
     int FRAME_WIDTH = 420;
     int FRAME_HEIGHT = 550;
@@ -32,7 +32,43 @@ public class Calculator implements ActionListener {
         textField.setFont(myFont);
         textField.setEditable(false);
 
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        eqButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        operationButtons[0] = addButton;
+        operationButtons[1] = subButton;
+        operationButtons[2] = mulButton;
+        operationButtons[3] = divButton;
+        operationButtons[4] = decButton;
+        operationButtons[5] = eqButton;
+        operationButtons[6] = delButton;
+        operationButtons[7] = clrButton;
+
+        for (JButton operationButton : operationButtons) {
+            operationButton.addActionListener(this);
+            operationButton.setFont(myFont);
+            operationButton.setFocusable(false);
+        }
+
+        for (int i = 0; i < numberButtons.length; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+
         frame.add(textField);
+        frame.add(delButton);
+        frame.add(clrButton);
 
         frame.setVisible(true);
 
