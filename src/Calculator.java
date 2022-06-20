74aa669fe8ece538ffcd2b40811c244969aed10f
jakegodsall <jake.edward.godsall@gmail.com>
2022-06-20 20:31:19 +0100

    added arrays of JButton's for operations and numbers

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] operationButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, eqButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 = 0;
+    double num2 =  0;
+    double result = 0;
+    char operator;
+
+    Calculator() {
+        frame = new JFrame("Calculator");
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
